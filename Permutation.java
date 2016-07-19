8237eb7d6f4e5c92a272c878d7b00b69bdc93124
metaamitasharma <amita.sharma@metacube.com>
2016-07-19 05:45:01 -0700

    Update Permutation.java

--- Permutation.java
+++ Permutation.java
@@ -4,7 +4,11 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Permutation {
-	ArrayList<String> as=new ArrayList<String>();
+	//to find all possible permutations of given string
+	
+	ArrayList<String> Str =new ArrayList<String>();
+	
+	//swaps ith char with jth in given character array a
 	void swap(char[] a, int i, int j) {
         char c = a[i];
         a[i] = a[j];
@@ -14,19 +18,23 @@ public class Permutation {
 	ArrayList<String> findpermutation(char[] a, int n) {
 	        if (n == 1)                                //base condition
 	        {
-	            String string=new String(a);
-	            as.add(string);
+	            String string=new String(a);           //converts char array to string
+	            Str.add(string);                       //adding string to the Arraylist
 	          //System.out.println(a);
 	            
 	        }
-	        for (int i = 0; i < n; i++) {
-	            swap(a, i, n-1);
-	            findpermutation(a, n-1);
+	        
+	                                                 //recursive calls:
+	        for (int i = 0; i < n; i++) {            //                          (abc,3)
+	            swap(a, i, n-1);                    //       (cba,2)            (acb,2)             (abc,2)
+	            findpermutation(a, n-1);         //    (bca,1)    (cba,1)   (cab,1)   (acb,1)   (bac,1)    (abc,1)   
 	            swap(a, i, n-1);
 	        }
 	      
 	        return as;
 	    }  
+	    
+	    
 public static void main(String[] args)
 {
 	Permutation obj = new Permutation();
